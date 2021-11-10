01be70ef543192cc8e6b85d344745c7f60d0b04e
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2021-11-10 20:20:44 +0800

    Modify User Table Schema

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'contact_number',
         'email',
         'password',
     ];
