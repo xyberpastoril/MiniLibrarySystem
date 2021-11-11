e3702422bf1c01d7af5159eaf99687082380ae98
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2021-11-11 16:09:47 +0800

    Establish book-author relationship

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,17 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Book extends Model
+class Author extends Model
 {
     use HasFactory;
 
-    public function book_genres()
-    {
-        return $this->hasMany(Book_Genres::class);
-    }
-
     public function book_authors()
     {
-        return $this->hasMany(Book_Authors::class);
+        return $this>belongsTo(Book_Authors::class);
     }
 }
