1c96bb8bc71e48f4b4122ed2c16b0a70baa4b7d8
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2021-11-12 08:49:46 +0800

    Remove contact number on schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('contact_number')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
