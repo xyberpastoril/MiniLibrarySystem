4e5f8fc56d5c66418c7f498bec4aaafbb339fe10
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2021-11-22 14:23:27 +0800

    Add total results and fix result responsiveness

--- public/js/search_results.js
+++ public/js/search_results.js
@@ -48,25 +48,29 @@ document.addEventListener('DOMContentLoaded', ()=>{
     })
 })
 generate_book_cards = (data) =>{
+    let inner = '';
     if (data.length==0){
-        return `Such Emptiness~`
+        inner += `<h3 class="fw-bold me-5 my-1">No results found.</h3>`
     }
-    let inner = '<div class="py-8 d-flex flex-row flex-wrap align-items-center justify-content-center w-100 position-relative h-auto">'
-    for(d of data){
-        inner+= `<div class="card card-block me-11 my-card mb-4" style="width: 14rem;flex: 0 0 auto;">
-        <img class="card-img-top" src="https://m.media-amazon.com/images/I/71ROjSv2ttL._AC_UY327_FMwebp_QL65_.jpg" alt="Book Cover" 
-            style="width: 100%; height: 225px; object-fit: cover;">
-        <div class="card-body p-2">
-            <p class="card-text text-truncate">
-                <a href="#" class="text-gray-800 text-hover-primary mb-1">${d.title}</a>
-                <br />
-                <small class="text-muted">
-                    ${d.authors.length>1? `${d.authors[0].name} and ${d.authors.length-1} others` : d.authors[0].name}
-                </small>
-            </p>
-        </div>
-    </div>`
+    else {
+        inner += `<h3 class="fw-bold me-5 my-1">${data.length} results found.</h3>`
+        inner += '<div class="py-8 d-flex flex-row flex-wrap align-items-center justify-content-center justify-content-lg-start w-100 position-relative h-auto">'
+        for(d of data){
+            inner+= `<div class="card card-block me-11 my-card mb-4" style="width: 14rem;flex: 0 0 auto;">
+            <img class="card-img-top" src="https://m.media-amazon.com/images/I/71ROjSv2ttL._AC_UY327_FMwebp_QL65_.jpg" alt="Book Cover" 
+                style="width: 100%; height: 225px; object-fit: cover;">
+            <div class="card-body p-2">
+                <p class="card-text text-truncate">
+                    <a href="#" class="text-gray-800 text-hover-primary mb-1">${d.title}</a>
+                    <br />
+                    <small class="text-muted">
+                        ${d.authors.length>1? `${d.authors[0].name} and ${d.authors.length-1} others` : d.authors[0].name}
+                    </small>
+                </p>
+            </div>
+        </div>`
+        }
+        inner += '</div>'
     }
-    inner += '</div>'
     return inner
 }
