3782e9c22706a315a50849f400969f7a7264bf73
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2021-11-22 20:32:48 +0800

    Renamed view `admin.users` to `admin.users.index`

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view("admin.users");
+        return view("admin.users.index");
     }
 
     /**
