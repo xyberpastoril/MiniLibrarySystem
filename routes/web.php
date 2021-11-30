5116bcf834a7034eaa1180ecba03fa7d6e6fc00a
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2021-11-30 10:28:48 +0800

    Add comments on `routes` to easily distinguish which is which

--- routes/web.php
+++ routes/web.php
@@ -30,15 +30,20 @@ Route::middleware(['auth'])->group(function(){
 
     });
 
+
+    // Books
     Route::resource('/books', App\Http\Controllers\BookController::class);
     Route::get('/result/books/search/', [App\Http\Controllers\BookController::class, 'search']);
 
+    // Users
     Route::resource('/users', App\Http\Controllers\UserController::class);
     Route::get('/result/users/search/', [App\Http\Controllers\UserController::class, 'search']);
 
     Route::get('/transactions/waiting_for_approval',[App\Http\Controllers\TransactionController::class,'waiting_for_approval'])->name('transactions.waiting_for_approval');
     Route::get('/transactions/in_progress',[App\Http\Controllers\TransactionController::class,'in_progress'])->name('transactions.in_progress');
     Route::get('/transactions/history',[App\Http\Controllers\TransactionController::class,'history'])->name('transactions.history');
+    // Transactions Pages
+    // Transactions JSON
     Route::get('/result/transactions/search/', [App\Http\Controllers\TransactionController::class, 'search']);
     
     Route::get('/account/overview',[App\Http\Controllers\AccountController::class,'overview'])->name('account.overview');
