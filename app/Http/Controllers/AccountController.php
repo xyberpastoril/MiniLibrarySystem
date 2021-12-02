5395dd20ce5b655f150cb0640fe3aa573aea3c4f
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2021-12-02 22:01:50 +0800

    Added validation to `updateEmail()`

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -18,14 +18,31 @@ class AccountController extends Controller
 
     public function updateEmail(Request $request)
     {
+        // Check if email already exists
+        if(\App\Models\User::where('email', '=', $request->email)->count() && $request->email != auth()->user()->email)
+        {
+            return [
+                'success' => 0,
+                'error' => "email_exists"
+            ];
+        }
+
         if(\Illuminate\Support\Facades\Hash::check($request->confirmemailpassword, auth()->user()->password))
         {
-            return \App\Models\User::where('id', '=', \Illuminate\Support\Facades\Auth::user()->id)
+            \App\Models\User::where('id', '=', \Illuminate\Support\Facades\Auth::user()->id)
             ->update([
                 'email' => $request->email
             ]);
+
+            return [
+                'success' => 1,
+                'error' => null
+            ];
         }
-        return 0;
+        return [
+            'success' => 0,
+            'error' => "wrong_password"
+        ];
     }
 
     public function updatePassword(Request $request)
