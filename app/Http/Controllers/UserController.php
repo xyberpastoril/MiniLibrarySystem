e19cdd4fd27f68e6cf96a9cd9c2156f83efc7688
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2021-12-04 15:49:34 +0800

    Show unverified members on `users.index`

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,7 @@ class UserController extends Controller
     public function index()
     {
         return view("admin.users.index", [
-            "allUsers" => \App\Models\User::search(null, 'member'),
+            "allUsers" => \App\Models\User::search(null, 'not_librarian'),
         ]);
     }
 
